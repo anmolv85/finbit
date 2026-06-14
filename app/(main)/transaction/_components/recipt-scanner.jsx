74ed7e86c58845e23999ec6973ffff985558d660
Anmol verma <anmolv8535@gmail.com>
2026-06-14 22:58:03 +0530

    Correct receipt scanner file size validation message

--- app/(main)/transaction/_components/recipt-scanner.jsx
+++ app/(main)/transaction/_components/recipt-scanner.jsx
@@ -23,8 +23,8 @@ export function ReceiptScanner({ onScanComplete }) {
   } = useFetch(scanReceipt);
 
   const handleReceiptScan = async (file) => {
-    if (file.size > 15 * 4000 * 4000) {
-      toast.error("File size should be less than 5MB");
+    if (file.size > 5 * 1024 * 1024) {
+      toast.error("File size should be less than 20MB");
       return;
     }
 
